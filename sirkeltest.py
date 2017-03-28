d461b38dba131cfc1ba2619f4ed37abca2970ec6
Patrik Sund <patriksu@stud.ntnu.no>
2017-03-28 14:13:49 +0200

    effektivisert

--- sirkeltest.py
+++ sirkeltest.py
@@ -1,14 +1,12 @@
-__author__ = 'Patrik'
-# -*- coding: utf-8 -*-
 import math
 import numpy as np
 from matplotlib import pyplot as plt
 X=[40,40]
-def f(X,t):
+def f(X,t): #Beregner w(i+1) med Eulers metode
     x=X[0]
     y=X[1]
     R=math.sqrt(x**2+y**2)
-    T=24*60*60
+    T=24*60*60 #setter perioden til 1 døgn i sekunder
     factor=2*math.pi/T
     Y=[x,y]
     Y[0]+=t*factor*(-y)
@@ -17,8 +15,7 @@ def f(X,t):
     y=Y[1]
     R=math.sqrt(x**2+y**2)
     return Y,R
-def findError\
-                (X,h):
+def plotThing(X,h):
     R=math.sqrt(X[0]**2+X[1]**2)
     R2=0
     x,y=X[0],X[1]
@@ -29,23 +26,23 @@ def findError\
     return error
 times=[30,60,4*60,8*60,15*60,30*60,3600,2*3600,4*3600,8*3600]
 errorArr=[]
-#print("X:",X)
+print("X:",X)
 for h in times:
-    print(findError
-          (X,h))
-    errorArr.append(findError
-                    (X,h))
-#print("X:",X)
-X=[0,100]
-h0=1
-#print("X:",X)
-while findError\
-            (X,h0)<10:
-    #print("X:",X)
-    print("error:",findError
-    (X,h0),"må være sant",findError
-    (X,h0)<10)
+    thiserror = plotThing(X,h)
+    print(thiserror)
+    errorArr.append(thiserror)
+print("X:",X)
+#Starter shit på nytt
+X=[40,40]
+h0=1 #sekund
+thiserror = plotThing(X,h0)
+while thiserror<10:
+    print("error:",thiserror,"må være sant",thiserror<10)
+    X=[40,40] #Hvorfor?
     h0+=1
+    thiserror = plotThing(X,h0)
 print("for å få en error større enn 10 meter må tidssteget være",h0,"sekunder")
 plt.loglog(times,errorArr)
+plt.xlabel("tid")
+plt.ylabel("error")
 plt.show()
